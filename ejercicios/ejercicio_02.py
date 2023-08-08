21c5777fc795fb0efbee43f2545bcdd63e70c026
CodeDasg <dsanabriagomez@outlook.es>
2023-08-08 13:36:36 -0500

    ejercicico 2 sin terminar

---
+++ ejercicios/ejercicio_02.py
@@ -0,0 +1,8 @@
+class Punto:
+
+    def __int__(self, coordenada_x: float, coordenada_y: float):
+        self.coordenada_x: float = coordenada_x
+        self.coordenada_y: float = coordenada_y
+
+    def representar_punto(self):
+        pass
