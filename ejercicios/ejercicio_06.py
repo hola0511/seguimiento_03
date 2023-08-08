db63cdce898de80d0e1ccf57a456fbb9476a81c3
CodeDasg <dsanabriagomez@outlook.es>
2023-08-08 12:59:03 -0500

    ejercicico 6

---
+++ ejercicios/ejercicio_06.py
@@ -0,0 +1,9 @@
+class Carta:
+    def __int__(self, valor: int, pinta):
+        self.valor: int = valor
+        self.pinta: str = pinta
+        self.picas = "picas"
+        self.trevor = "trevor"
+        self.corazon = "corazon"
+        self.diamante = "diamante"
+
