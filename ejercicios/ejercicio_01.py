3d1a634c625e7930973e6032cae05ef9da580a69
CodeDasg <dsanabriagomez@outlook.es>
2023-08-08 12:54:17 -0500

    ejercicico 1 y licencia

---
+++ ejercicios/ejercicio_01.py
@@ -0,0 +1,5 @@
+class Vehiculo:
+    def __init__(self, velocidad_maxima: float, kilometraje: int):
+        self.velocidad_maxima: float = velocidad_maxima
+        self.kilometraje: int = kilometraje
+        
